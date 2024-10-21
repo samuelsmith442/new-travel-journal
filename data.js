16967d9c767a2cf10f84310b8c1a95b9e6ca4a19
Samuel Smith <10069706+samuelsmith442@users.noreply.github.com>
2024-10-21 00:45:59 -0400

    initial commit
    project of world travels

---
+++ data.js
@@ -0,0 +1,32 @@
+export default [
+    {
+        id: 1,
+        title: "Mount Fuji",
+        location: "Japan",
+        googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
+        startDate: "12 Jan, 2021",
+        endDate: "24 Jan, 2021",
+        description: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
+        imageUrl: "https://images.unsplash.com/photo-1705481109538-fb7edf4a5758?q=80&w=3870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    },
+    {
+        id: 2,
+        title: "Sydney Opera House",
+        location: "AUSTRALIA",
+        googleMapsUrl: "https://maps.app.goo.gl/9PeZSvqz9m5MqpVw7",
+        startDate: "27 May, 2021",
+        endDate: "8 June, 2021",
+        description: "The Sydney Opera House is a multi-venue performing arts centre in Sydney. Located on the banks of the Sydney Harbour, it is often regarded as one of the 20th century's most famous and distinctive buildings.",
+        imageUrl: "https://images.unsplash.com/photo-1523059623039-a9ed027e7fad?q=80&w=3832&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    },
+    {
+        id: 3,
+        title: "Geirangerfjord",
+        location: "NORWAY",
+        googleMapsUrl: "https://maps.app.goo.gl/Uyu9aSqpq8sGiiE47",
+        startDate: "01 Oct, 2021",
+        endDate: "18 Nov, 2021",
+        description: "The Geiranger Fjord is a fjord in the Sunnmøre region of Møre og Romsdal county, Norway. It is located entirely in the Stranda Municipality.",
+        imageUrl: "https://images.unsplash.com/photo-1705512604302-3dc9d171832c?q=80&w=3764&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    }
+]
